ff4c80b24361c76238495d49b99cb91b448ebc8d
Andy Uzal <andyuzal@gmail.com>
2022-11-23 11:59:53 +0100

    Primer subida de proyecto

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Funciones de redireccionamiento
+
+function redireccionarPagina(){
+    location.href = "./pages/formulario.html";
+}
+
+function NuevaTemporada(){
+    location.href = "./pages/nuevatemporada.html";
+}
+
+function Index(){
+    location.href = "../index.html";
+}
+
+function NosotrosPage(){
+    location.href = "./pages/nosotros.html";
+}
+
+function BolsosPage() {
+    location.href = "./pages/bolsos.html";
+}
+
+function SosteniblePage(){
+    location.href = "./pages/sostenible.html";
+}
+
+function MaletasPage(){
+    location.href = "./pages/maletas.html";
+}
+
+function CarterasPage(){
+    location.href = "./pages/carteras.html";
+}
+
+function OutletPage(){
+    location.href = "./pages/outlet"
+}
